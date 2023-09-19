2435732d3cc28dd58af51f1ed4488252d8c5a5c8
elliotls <elliotlinessmith@elliots-mbp.lan>
2023-09-19 16:44:48 +1000

    Iterate over user data and insert into DB

--- user_upload.php
+++ user_upload.php
@@ -1,15 +1,25 @@
 #!/usr/bin/env php
 <?php
+
 function readInputFile() {
     $cmdOptions = ["file:","dry_run:"];
     $cmdValues = getopt('file:u:p:h:', $cmdOptions);
-    
+
+    // Validate file format
+    if (!isset($cmdValues['file']) || !str_ends_with($cmdValues['file'], ".csv")) {
+        echo "Please provide a csv file with the --file option\n";
+        exit;
+    } else {
+        echo "Filename: {$cmdValues['file']}\n";
+    }
+
     $csv = array_map("str_getcsv", file($cmdValues['file'], FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES));
     $header = array_shift($csv);
     if ($header[0] != "name" || $header[1] != "surname" || trim($header[2]) != "email") {
         echo "Invalid file format:\n";
         exit;
     }
+
     return [$cmdValues, $csv];
 }
 
@@ -38,7 +48,11 @@ function initialiseDatabase($user, $password, $host) {
 
     $sql = "DROP DATABASE IF EXISTS user_upload;
             CREATE DATABASE user_upload;
-            CREATE TABLE user_upload.users;";
+            CREATE TABLE user_upload.users (
+                name VARCHAR(100),
+                surname VARCHAR(100),
+                email VARCHAR(100) UNIQUE
+            );";
 
     if ($databaseConnection->multi_query($sql) === TRUE) {
         echo "Database created successfully with the name user_upload\n";
@@ -50,6 +64,39 @@ function initialiseDatabase($user, $password, $host) {
     return;
 }
 
+function insertUsers($csvRows, $host, $user, $password) {
+    echo "Beginning user insert\n";
+    $queryArray = [];
+    $queuedEmailAddresses = [];
+    $databaseConnection = new mysqli($host, $user, $password, 'user_upload');
+    foreach ($csvRows as $userData) {
+        $userData[0] = str_replace('\'', '\\\'', $userData[0]);
+        $userData[1] = str_replace('\'', '\\\'', $userData[1]);
+        $emailAddress = trim(str_replace('\'', '\'\'', $userData[2]));
+        // Validate and queue email address
+        if (!validateEmailAddress($emailAddress)) {
+            echo "Email address '$emailAddress' is considered invalid and will be ignored.\n";
+        } elseif (in_array($emailAddress, $queuedEmailAddresses)) {
+            echo "Email address '$emailAddress' is not unique and will be ignored.\n";
+        } else {
+            $queryArray[] = "('$userData[0]', '$userData[1]', '$emailAddress')";
+            $queuedEmailAddresses[] = $emailAddress;
+        }
+    }
+    
+    // echo "INSERT INTO users (name, surname, email) VALUES ".implode(",",$queryArray)."\n";
+    $result = $databaseConnection->query("INSERT INTO users (name, surname, email) VALUES ".implode(",",$queryArray));
+    if (!$result) {
+        die('Invalid query');
+    }
+    echo "Database populated. Closing connection\n";
+    $databaseConnection->close();
+}
+
+function validateEmailAddress($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
 echo "~~ Initialising user upload ~~ \n";
 [$cmdValues, $csvRows] = readInputFile();
 echo "CSV Rows: {$csvRows[0][0]}, {$csvRows[0][1]}, {$csvRows[0][2]}\n";
@@ -58,6 +105,6 @@ validateDatabaseCreds($cmdValues);
 
 initialiseDatabase($cmdValues['u'], $cmdValues['p'], $cmdValues['h']);
 
-// Iterate over file contents validating and prepping for insert
+insertUsers($csvRows, $cmdValues['h'], $cmdValues['u'], $cmdValues['p']);
 
 ?>
